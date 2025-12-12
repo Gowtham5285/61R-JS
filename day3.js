edbb405516d8d4bf561cdec3a5f2bb8cb90cc67f
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-12-12 11:13:26 +0530

    Added CRUD

--- day3.js
+++ day3.js
@@ -16,8 +16,34 @@ fetch("http://localhost:3000/quotes")
         btn.textContent="Delete"
         div.append(h1,h3,p,btn)
         container.appendChild(div)
+        btn.addEventListener("click",function(){
+            // console.log(item.id)
+            deleteData(item.id)
+        })
 
     }
 })
 }
 getData()
+function deleteData(id){
+    // console.log(id)
+    fetch(`http://localhost:3000/quotes/${id}`,{
+        method:"DELETE",
+
+    })
+    .then(res=>res.json())
+    .then(data=>{console.log(data)
+    fetch("http://localhost:3000/delete",{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    }).then(res=>{
+        if(res.ok){
+            console.log("Data is submitted in the deleted Sucessfully......")
+             getData()
+        }
+    })})
+   
+}
