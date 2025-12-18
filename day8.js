41d5b671c70c8b210531e7aa934bb91be06e484f
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-12-18 10:36:12 +0530

    Async & Await

--- day8.js
+++ day8.js
@@ -17,3 +17,48 @@
 //     }
 // }
 // API_fetch()
+let div = document.getElementById("div");
+
+async function abc() {
+    const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'YOUR_API_KEY',
+            'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        if (!result.data || !Array.isArray(result.data)) {
+            console.error("Invalid data format:", result);
+            return;
+        }
+
+        let ul = document.createElement("ul");
+
+        result.data.forEach(i => {
+            let li = document.createElement("li");
+            li.innerHTML = `
+                <h3>${i.title}</h3>
+                <img src="${i.image}" width="150" />
+            `;
+            ul.appendChild(li);
+        });
+
+        div.appendChild(ul);
+
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+abc();
